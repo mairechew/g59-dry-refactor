891f3811105f636336d76f56da57fa513ea0740c
Dan Levy <dan@danlevy.net>
2017-11-22 09:33:56 -0700

    solution mostly complete!!!

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,8 @@
 const knex = require('../lib/knex')
 const express = require('express')
 const router = express.Router()
-const {find, create, deleteById, validateId} = require('../lib/books')
+const {find, create, update,
+  deleteById, validateId} = require('../lib/books')
 
 router.get('/', (req, res, next) => {
   find()
@@ -10,63 +11,28 @@ router.get('/', (req, res, next) => {
 })
 
 router.get('/:id', (req, res, next) => {
-  // make sure that id is a number,
-  // if it is not a number, send a 400 (Bad Request)
   find(req.params.id)
     .then(data => res.send(data))
     .catch(err => res.status(500).send(err))
 })
 
 router.post('/', (req, res, next) => {
-  // Make sure that the information that you want is in the request body.
   create(req.body, '*')
     .then(books => res.send(books[0]))
-    .catch(err => next(err))
+    .catch(err => res.status(500).send(err))
 })
 
 router.patch('/:id', (req, res, next) => {
-  // Make sure that id is a number
-
-  knex('books')
-    .where('id', req.params.id)
-    .first()
+  update(req.params.id)
     .then(book => {
-      if (!book) {
-        return next()
-      }
-
-      return knex('books')
-        .update(
-          {
-            title: req.body.title,
-            author: req.body.author,
-            genre: req.body.genre,
-            description: req.body.description,
-            cover_url: req.body.coverUrl,
-          },
-          '*'
-        )
-        .where('id', req.params.id)
+      if (!book) return next()
+      res.send(book)
     })
-    .then(books => {
-      const newObj = {
-        id: books[0].id,
-        title: books[0].title,
-        author: books[0].author,
-        genre: books[0].genre,
-        description: books[0].description,
-        coverUrl: books[0].cover_url,
-      }
-      res.send(newObj)
-    })
-    .catch(err => {
-      next(err)
-    })
+    .catch(err => res.status(500).send(err))
 })
 
 router.delete('/:id', (req, res, next) => {
   let book = null
-  // Make sure that id is a number
   validateId(req.params)
     .then(find)
     .then(data => {
